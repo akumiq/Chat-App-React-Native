6d388b3ba5a7a089f24aa14a4ec06f629b62ceea
Miqdad <kun.miqdad23@gmail.com>
2020-10-19 10:08:42 +0700

    create database for chats

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import {LogBox, View, Text} from 'react-native';
+import React, {useState, useEffect, useCallback} from 'react';
+import {LogBox, SafeAreaView} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import * as firebase from 'firebase';
 import 'firebase/firestore';
+import AsyncStorage from '@react-native-community/async-storage';
+import {GiftedChat} from 'react-native-gifted-chat';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDWcfQID0ZRdiHhthXeLGSwDHpdgAuNUyc',
@@ -25,10 +27,52 @@ const chatRef = db.collection('chats');
 LogBox.ignoreLogs(['Setting a timer for a long period of time']);
 
 const ChatScreen = () => {
+  const [user, setUser] = useState(null);
+  const [message, setMessage] = useState([]);
+
+  useEffect(() => {
+    readUser();
+    const unSubscribe = chatRef.onSnapshot((querySnapshot) => {
+      const messageFirestore = querySnapshot
+        .docChanges()
+        .filter(({type}) => type === 'added')
+        .map(({doc}) => {
+          const messages = doc.data();
+          return {...messages, createdAt: messages.createdAt.toDate()};
+        })
+        .sort(({a, b}) => b.createdAt.getTime() - a.createdAt.getTime());
+
+      appendMessage(messageFirestore);
+    });
+
+    return () => unSubscribe();
+  }, [appendMessage]);
+
+  const readUser = async () => {
+    const users = await AsyncStorage.getItem('user');
+    if (users) {
+      setUser(JSON.parse(users));
+    }
+  };
+
+  const appendMessage = useCallback((messages) => {
+    setMessage((prev) => GiftedChat.append(prev, messages));
+  }, []);
+
+  const onHandleSend = async (messages) => {
+    const writes = messages.map((m) => chatRef.add(m));
+    await Promise.all(writes);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.textTitle}>Ini Chat Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <GiftedChat
+        key={(index) => index.toString()}
+        onSend={onHandleSend}
+        messages={message}
+        user={user}
+      />
+    </SafeAreaView>
   );
 };
 
@@ -38,9 +82,5 @@ const styles = EStyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
-    justifyContent: 'center',
-  },
-  textTitle: {
-    textAlign: 'center',
   },
 });
