4269f21a834679aa0392dfa957e0068f0eacce29
Miqdad <kun.miqdad23@gmail.com>
2020-10-19 09:03:38 +0700

    setup config firebase

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,6 +1,28 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {LogBox, View, Text} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDWcfQID0ZRdiHhthXeLGSwDHpdgAuNUyc',
+  authDomain: 'chat-app-e6405.firebaseapp.com',
+  databaseURL: 'https://chat-app-e6405.firebaseio.com',
+  projectId: 'chat-app-e6405',
+  storageBucket: 'chat-app-e6405.appspot.com',
+  messagingSenderId: '347009828405',
+  appId: '1:347009828405:web:2fcda68202c9ec69b7d37c',
+  measurementId: 'G-381EC7QMF5',
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+const chatRef = db.collection('chats');
+
+LogBox.ignoreLogs(['Setting a timer for a long period of time']);
 
 const ChatScreen = () => {
   return (
@@ -15,6 +37,7 @@ export default ChatScreen;
 const styles = EStyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#ffffff',
     justifyContent: 'center',
   },
   textTitle: {
